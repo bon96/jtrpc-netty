16764aed0a085d4d8abc9794eeaa2b868a7fedff
bon <bor.monty@gmail.com>
2020-05-04 12:30:16 +0300

    Update ExampleServer.java

--- src/test/java/ExampleServer.java
+++ src/test/java/ExampleServer.java
@@ -6,7 +6,6 @@ public class ExampleServer {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         server.start();
-        System.setProperty("source", "server");
         server.registerMethods(Command2.class);
     }
 }
