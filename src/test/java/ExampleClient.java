2a0323ba788511b14ecceae2eff843faf7d70964
tommi <->
2022-06-26 22:18:27 +0300

    useful comments on what's happening

--- src/test/java/ExampleClient.java
+++ src/test/java/ExampleClient.java
@@ -4,14 +4,15 @@ import org.bonbom.Client;
 public class ExampleClient {
 
     public static void main(String[] args) throws Exception {
-        Client client = new Client("localhost", 8080);
-        client.start();
-        Command command = client.createProxy(Command.class);
-        System.out.println(command.sayHiToServer("Client says hi!"));
+        Client client = new Client("localhost", 8080); //initiate client parameters
+        client.start(); //Connect client to server
 
-        System.setProperty("source", "client");
+        Command command = client.createProxy(Command.class); //create a proxy class of which all method calls are executed on server
+        System.out.println(command.sayHiToServer("Client says hi!")); //prints message on server and server returns a value as response
 
-        command.run(() -> System.out.println(System.getProperty("source")));
-        client.stop();
+        System.setProperty("source", "client"); //for testing if server or client executes the code below
+
+        command.run(() -> System.out.println(System.getProperty("source"))); //execute lambda function on the server
+        client.stop(); //stop the client
     }
 }
--- src/test/java/ExampleServer.java
+++ src/test/java/ExampleServer.java
@@ -5,10 +5,12 @@ import org.bonbom.Server;
 
 public class ExampleServer {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
-        server.start();
-        System.setProperty("source", "server");
-        server.registerMethods(Command.class, CommandImpl.class);
+        Server server = new Server(8080); //Initiate server parameters
+        server.start(); //start server and begin accepting client connections
+
+        System.setProperty("source", "server"); //set source to server for the client test example
+
+        server.registerMethods(Command.class, CommandImpl.class); //register Command.class methods and link them to CommandImpl class
     }
 }
 
